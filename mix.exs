2d037eeb04b22bcb987392f26262ba1c8b14acc9
Col Harris <col.w.harris@gmail.com>
2016-04-16 22:24:44 +0800

    Cleanup

--- mix.exs
+++ mix.exs
@@ -10,9 +10,6 @@ defmodule EchoBot.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [
       applications: [:logger, :nadia, :gproc, :edeliver],
@@ -20,23 +17,12 @@ defmodule EchoBot.Mixfile do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:nadia, "~> 0.4"},
       {:gproc, "0.5.0"},
       {:poison, "~> 2.0", override: true},
-      {:telegram, git: "https://github.com/col/telegram"},
-      # {:exrm, "~> 1.0.3"},
-      {:edeliver, "~> 1.1.5"}
+      {:telegram, git: "https://github.com/col/telegram"}
     ]
   end
 end
