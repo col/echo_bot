5328b40d70eb78028eacf9a0ab74d518f7a0e306
Col Harris <col.w.harris@gmail.com>
2016-04-18 12:36:30 +0800

    Try to fix Snap CI error "mix: command not found"

--- snap_ci_release.sh
+++ snap_ci_release.sh
@@ -1,2 +1,6 @@
+export MY_DEPS_PATH=/var/go/deps
+source ~/.kerl/installs/18.2.1/activate
+export PATH=`pwd`/vendor/elixir/bin:$PATH
+
 echo "Build release..."
 mix edeliver build release
--- snap_ci_test.sh
+++ snap_ci_test.sh
@@ -1,2 +1,6 @@
+export MY_DEPS_PATH=/var/go/deps
+source ~/.kerl/installs/18.2.1/activate
+export PATH=`pwd`/vendor/elixir/bin:$PATH
+
 echo "Run Elixir tests..."
 MIX_ENV=test mix test
